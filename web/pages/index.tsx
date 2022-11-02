056ef663b3eba93f3b112392478a9d0e8d75d62e
unknown <joaogabriel1412@gmail.com>
2022-11-02 15:36:45 -0300

    Create web

---
+++ web/pages/index.tsx
@@ -0,0 +1,23 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+
+  return (
+    <h1>Hello Worldd: {props.count}</h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+  
+  const data = await response.json();
+
+
+  return {
+    props: {
+      count: data.count
+    }
+  }
+}
