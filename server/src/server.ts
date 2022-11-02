ede1c645ec8039ae2faaee7c536ad3a209add363
unknown <joaogabriel1412@gmail.com>
2022-11-02 17:02:32 -0300

    Create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,12 @@ import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 
+// Biblioteca para a geração de unique ids
+import ShortUniqueId from 'short-unique-id';
+
+// Biblioteca para validação de schema vindo da requisição (Semelhante ao Joi)
+import { z } from 'zod';
+
 const prisma = new PrismaClient({
     log: ['query']
 })
@@ -16,10 +22,43 @@ async function bootstrap(){
     })
 
     fastify.get('/pools/count', async () => {
-
         const count = await prisma.pool.count();
 
         return { count }
+    })
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count }
+    })
+
+    fastify.post('/pools', async (req, res) => {
+        // Criação do schema
+        const createPoolBody = z.object({
+            title: z.string(),
+        })
+
+        const { title } = createPoolBody.parse(req.body);
+
+        // Generate unique id
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return res.status(201).send({ code })
     })
 
     await fastify.listen({port: 3333, host: '0.0.0.0'});
