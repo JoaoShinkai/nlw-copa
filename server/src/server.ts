6e5591cdcb2881e09e6c075c11dcedd69e68afbd
unknown <joaogabriel1412@gmail.com>
2022-11-04 17:10:46 -0300

    backend finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,13 @@
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import jwt from '@fastify/jwt';
 import Fastify from 'fastify';
 
-// Biblioteca para a geração de unique ids
-import ShortUniqueId from 'short-unique-id';
+import { authRoutes } from './routes/auth.routes';
+import { gameRoutes } from './routes/game.routes';
+import { guessRoutes } from './routes/guess.routes';
 
-// Biblioteca para validação de schema vindo da requisição (Semelhante ao Joi)
-import { z } from 'zod';
-
-const prisma = new PrismaClient({
-    log: ['query']
-})
+import { poolRoutes } from './routes/pool.routes';
+import { userRoutes } from './routes/user.routes';
 
 async function bootstrap(){
     const fastify = Fastify({
@@ -21,45 +18,16 @@ async function bootstrap(){
         origin: true
     })
 
-    fastify.get('/pools/count', async () => {
-        const count = await prisma.pool.count();
-
-        return { count }
+    await fastify.register(jwt, {
+        secret: 'nlwcopasecretkey'
     })
 
-    fastify.get('/users/count', async () => {
-        const count = await prisma.user.count();
+    await fastify.register(authRoutes);
+    await fastify.register(gameRoutes);
+    await fastify.register(guessRoutes);
+    await fastify.register(poolRoutes);
+    await fastify.register(userRoutes);
 
-        return { count }
-    })
-
-    fastify.get('/guesses/count', async () => {
-        const count = await prisma.guess.count();
-
-        return { count }
-    })
-
-    fastify.post('/pools', async (req, res) => {
-        // Criação do schema
-        const createPoolBody = z.object({
-            title: z.string(),
-        })
-
-        const { title } = createPoolBody.parse(req.body);
-
-        // Generate unique id
-        const generate = new ShortUniqueId({ length: 6 });
-        const code = String(generate()).toUpperCase();
-
-        await prisma.pool.create({
-            data: {
-                title,
-                code
-            }
-        })
-
-        return res.status(201).send({ code })
-    })
 
     await fastify.listen({port: 3333, host: '0.0.0.0'});
 }
